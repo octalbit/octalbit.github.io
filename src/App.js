61cdf6040f5608d349572e411f6c07d92238be12
rafaelangical <rsnrafael@outlook.com>
2017-08-30 18:47:11 -0300

    New version

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Hero from './Hero'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Hero />
+      </div>
+    );
+  }
+}
+
+export default App;
