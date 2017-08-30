94b4c3e06ff1dc797d14dc15c87de28c3cc02d88
rafaelangical <rsnrafael@outlook.com>
2017-08-30 13:13:45 -0300

    add CNAME

---
+++ build.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+TMP_DIR=$(mktemp -d)
+
+echo "\033[1;33m What have you done to the site?\033[0m"
+read update
+
+# Create bundle
+yarn run build
+git commit -am "Create new bundle"
+
+# Copy build to temp folder
+cp -r ./build/ $TMP_DIR/build
+BUILD_FILES=`ls ./build`
+
+# Wipe out current site
+git checkout master
+git rm -rf .
+mv .git $TMP_DIR/.git
+rm -rf .
+mv $TMP_DIR/.git .git
+
+# Move new bundle to master
+mv -f $TMP_DIR/build/** ./
+
+# Create CNAME
+echo "octalbit.com" > CNAME
+git add CNAME
+git add $BUILD_FILES
+
+# Commit and push changes
+git commit -m "$update"
+echo "\033[1;31mPushing new site to GitHub repo\n\033[0m"
+git push --force origin master
+
+# Get back to develop
+git checkout develop
